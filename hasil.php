aa505b46455f516d2331c3cdad05aba15e2c5051
sahistyapratita <sahistyapratita6@gmail.com>
2023-09-28 22:28:31 +0700

    update bg dan title

--- hasil.php
+++ hasil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hasil Tugas 3</title>
+    <title>Biann</title>
     <style>
         *{
             margin: 0;
@@ -45,7 +45,7 @@
         }
     </style>
 </head>
-<body>
+<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
 
     <div class="container-hasiltugas3">
         <h1>Your Input</h1>
